8508e52f2e5d097f5f750e3acd9101925a554060
unknown <Daniel Rody>
2022-12-09 19:16:10 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "180d38ec90d8e8eda79eacde11de46a5";
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
